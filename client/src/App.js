758273e6c90271547965c3d20869619ecfa41164
rchvalbo <28923633+rchvalbo@users.noreply.github.com>
2018-09-24 17:21:11 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
-<<<<<<< HEAD
 /* Once the 'Authservice' and 'withAuth' componenets are created, import them into App.js */
 import AuthHelperMethods from './components/AuthHelperMethods';
 
 //Our higher order component
 import withAuth from './components/withAuth';
 
-=======
->>>>>>> 40702bb99251b06a8010e4a0f32f7ca04bae26fa
-
 class App extends Component {
 
   /* Create a new instance of the 'AuthHelperMethods' compoenent*/
-<<<<<<< HEAD
+
   Auth = new AuthHelperMethods();
-=======
->>>>>>> 40702bb99251b06a8010e4a0f32f7ca04bae26fa
+
 
   state = {
     username: "",
@@ -26,12 +21,10 @@ class App extends Component {
 
 /* Here will want to add a method to log the user out upon clicking 'Logout' */
   _handleLogout = () => {
-<<<<<<< HEAD
+
     this.Auth.logout()
     this.props.history.replace('/login');
-=======
-    
->>>>>>> 40702bb99251b06a8010e4a0f32f7ca04bae26fa
+
   }
 
   //Render the protected component
@@ -55,8 +48,6 @@ class App extends Component {
 }
 
 //In order for this component to be protected, we must wrap it with what we call a 'Higher Order Component' or HOC.
-<<<<<<< HEAD
+
 export default withAuth(App);
-=======
-export default App;
->>>>>>> 40702bb99251b06a8010e4a0f32f7ca04bae26fa
+
